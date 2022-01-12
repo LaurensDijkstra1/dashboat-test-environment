d6825565d50ceeeb2bc26f3b4d6820d5167a525d
Laurens Dijkstra <laurensdijkstra1@gmail.com>
2022-01-12 11:41:58 +0100

    first test version

---
+++ _nuxt/49f1b4a.js
@@ -0,0 +1,2 @@
+/*! For license information please see LICENSES */
+(window.webpackJsonp=window.webpackJsonp||[]).push([[16],{288:function(e,n,r){"use strict";r.r(n);var o=r(279);r.d(n,"default",(function(){return o.a}));o.a.registerVersion("firebase","9.6.1","app-compat")}}]);
---
+++ _nuxt/89318ab.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[12],{291:function(n,t,e){"use strict";e.r(t);var c={asyncData:function(){$nuxt.$fire.auth.signOut()}},l=e(27),component=Object(l.a)(c,(function(){var n=this,t=n.$createElement;return(n._self._c||t)("h1",[n._v("Logged Out")])}),[],!1,null,null,null);t.default=component.exports}}]);
